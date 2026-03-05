71ade21d88f521c01f2bd9a9eeb9bda82be08fc0
Faure, P.G. (Pierre) <pierre.faure@lalessels.com>
2026-03-05 08:35:20 +0100

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,7 @@
+rootProject.name = "kozsketch"
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+}
---
+++ src/main/kotlin/HTTP.kt
@@ -0,0 +1,24 @@
+package com
+
+import io.ktor.http.*
+import io.ktor.openapi.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.calllogging.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.plugins.swagger.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import java.sql.Connection
+import java.sql.DriverManager
+import org.jetbrains.exposed.sql.*
+import org.slf4j.event.*
+
+fun Application.configureHTTP() {
+    routing {
+        swaggerUI(path = "openapi") {
+            info = OpenApiInfo(title = "My API", version = "1.0.0")
+        }
+    }
+}
