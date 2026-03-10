82ab530c379473aed06b3331d57e858a316c2cb3
Faure, C. (Chloé) <pierre.faure@lalessels.com>
2026-03-10 15:44:17 +0100

    lil extra refactor

--- src/test/kotlin/acceptanceTests/projects/ProjectsAT.kt
+++ src/test/kotlin/acceptanceTests/projects/ProjectsAT.kt
@@ -6,14 +6,14 @@ import com.helpers.constants.DEFAULT_PROJECT_RATING
 import com.helpers.constants.PROJECTS_URL
 import com.helpers.constants.UPDATED_PROJECT_NAME
 import com.helpers.constants.UPDATED_PROJECT_RATING
-import com.helpers.extensions.createEnvironment
+import com.helpers.extensions.runAT
 import com.infrastructure.Project
-import com.module
 import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.core.spec.style.scopes.BehaviorSpecWhenContainerScope
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
+import io.ktor.client.HttpClient
 import io.ktor.client.call.body
-import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.request.delete
 import io.ktor.client.request.get
 import io.ktor.client.request.post
@@ -21,17 +21,19 @@ import io.ktor.client.request.put
 import io.ktor.client.request.setBody
 import io.ktor.client.statement.bodyAsText
 import io.ktor.http.ContentType
-import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpStatusCode
 import io.ktor.http.contentType
-import io.ktor.serialization.kotlinx.json.json
-import io.ktor.server.testing.testApplication
 import java.util.UUID
 
 class ProjectsAT : BehaviorSpec({
+    projectDoesNotYetExistTests()
+    projectAlreadyExistsTests()
+})
+
+private fun BehaviorSpec.projectDoesNotYetExistTests() {
     Given("a project does not yet exist") {
         When("a new project is created") {
-            testApplication { createEnvironment()
+            runAT {
                 val createResponse = client.post(PROJECTS_URL) {
                     contentType(ContentType.Application.Json)
                     setBody(ProjectBuilder().createDefault())
@@ -43,7 +45,8 @@ class ProjectsAT : BehaviorSpec({
                 }
                 Then("we should be able to get the project") {
                     val project = client.get(
-                            "$PROJECTS_URL/$projectId")
+                        "$PROJECTS_URL/$projectId"
+                    )
                         .body() as Project
 
                     project shouldNotBe null
@@ -52,39 +55,40 @@ class ProjectsAT : BehaviorSpec({
                 }
                 And("we try to update the project") {
                     val updateResponse = client.put(
-                        "$PROJECTS_URL/$projectId"){
+                        "$PROJECTS_URL/$projectId"
+                    ) {
                         contentType(ContentType.Application.Json)
-                        setBody(ProjectBuilder().createWithValidProperties(
-                            UPDATED_PROJECT_NAME,
-                            UPDATED_PROJECT_RATING
-                        ))
+                        setBody(
+                            ProjectBuilder().createWithValidProperties(
+                                UPDATED_PROJECT_NAME,
+                                UPDATED_PROJECT_RATING
+                            )
+                        )
                     }
 
                     Then("we should receive HTTP.OK") {
                         updateResponse.status shouldBe HttpStatusCode.OK
                     }
                     And("we get the project again") {
-                        val updatedProject = client.get(
-                            "$PROJECTS_URL/$projectId")
-                            .body() as Project
-
-                        Then("the project should be updated") {
-                            updatedProject shouldNotBe null
-                            updatedProject.name shouldBe UPDATED_PROJECT_NAME
-                            updatedProject.rating shouldBe UPDATED_PROJECT_RATING
-                        }
+                        projectShouldBeUpdated(
+                            client,
+                            projectId,
+                            this@And
+                        )
                     }
                 }
                 And("we try to delete the project") {
                     val deleteResponse = client.delete(
-                        "$PROJECTS_URL/$projectId")
+                        "$PROJECTS_URL/$projectId"
+                    )
 
                     Then("we should receive HTTP.OK") {
                         deleteResponse.status shouldBe HttpStatusCode.OK
                     }
                     And("we try to get the project again") {
                         val getResponse = client.get(
-                            "$PROJECTS_URL/$projectId")
+                            "$PROJECTS_URL/$projectId"
+                        )
 
                         Then("we should receive 404 Not Found") {
                             getResponse.status shouldBe HttpStatusCode.NotFound
@@ -94,9 +98,30 @@ class ProjectsAT : BehaviorSpec({
             }
         }
     }
+}
+
+//TODO: might extract this to separate class?
+private suspend fun projectShouldBeUpdated(
+    client: HttpClient,
+    projectId: UUID?,
+    scope: BehaviorSpecWhenContainerScope
+) {
+    val updatedProject = client.get(
+        "$PROJECTS_URL/$projectId"
+    )
+        .body() as Project
+
+    scope.Then("the project should be updated") {
+        updatedProject shouldNotBe null
+        updatedProject.name shouldBe UPDATED_PROJECT_NAME
+        updatedProject.rating shouldBe UPDATED_PROJECT_RATING
+    }
+}
+
+private fun BehaviorSpec.projectAlreadyExistsTests() {
     Given("a project already exists") {
         When("a project with the same name is created") {
-            testApplication { createEnvironment()
+            runAT {
                 client.post(PROJECTS_URL) {
                     contentType(ContentType.Application.Json)
                     setBody(ProjectBuilder().createDefault())
@@ -112,4 +137,4 @@ class ProjectsAT : BehaviorSpec({
             }
         }
     }
-})
+}
--- src/test/kotlin/helpers/extensions/ApplicationTestBuilderExtensions.kt
+++ src/test/kotlin/helpers/extensions/ApplicationTestBuilderExtensions.kt
@@ -1,9 +1,12 @@
 package com.helpers.extensions
 
 import com.module
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.core.spec.style.scopes.ContainerScope
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.testing.ApplicationTestBuilder
+import io.ktor.server.testing.testApplication
 
 fun ApplicationTestBuilder.createEnvironment() {
     application {
@@ -13,5 +16,14 @@ fun ApplicationTestBuilder.createEnvironment() {
         install(ContentNegotiation) {
             json()
         }
+    }
+}
+
+fun runAT(
+    body: suspend ApplicationTestBuilder.() -> Unit
+) {
+    testApplication {
+        createEnvironment()
+        body()
     }
 }
