3f301328cd1b23de5f84f9ffbf3b80a7f02e76f6
Chloé Faure <p.g.faure@hotmail.com>
2026-03-14 19:16:43 +0100

    Refactor DatabaseConnection, add at.yaml (and hope it works in the build pipeline)

--- src/main/kotlin/plugins/Koin.kt
+++ src/main/kotlin/plugins/Koin.kt
@@ -6,13 +6,10 @@ import com.repositories.di.repositoryModule
 import com.services.di.serviceModule
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
-import io.ktor.server.routing.IgnoreTrailingSlash
 import org.koin.ktor.plugin.Koin
 import org.koin.logger.slf4jLogger
 
 fun Application.configureKoin() {
-    install(IgnoreTrailingSlash)
-
     install(Koin) {
         slf4jLogger()
         modules(
